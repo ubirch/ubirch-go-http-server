503018d57105571e5838b8b42e3e4e2703416138
Roxana Meixner <roxana.meixner@gmail.com>
2020-03-23 19:45:56 +0100

    create compact sorted rendering of json request body

--- api/ubirch-rest-api.go
+++ api/ubirch-rest-api.go
@@ -1,43 +1,73 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
 )
 
+func stringInList(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 func handleRequest(requestChan chan []byte, responseChan chan Response) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// only accept POST requests
+		if r.Method != "POST" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte(`{"message": "http method not implemented"}`))
+		}
+
+		// read request body
 		reqBody, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Printf("error reading http request body: %v", err)
 			return
 		}
 
-		if r.Method == "POST" {
-			// get the request body
-			log.Println(reqBody)
-			requestChan <- reqBody
-
-			// wait for response from ubirch backend to be forwarded
-			select {
-			case resp := <-responseChan:
-				w.WriteHeader(resp.Code)
-				w.Write(resp.Content)
+		//
+		if stringInList("application/json", r.Header["Content-Type"]) {
+			// make a sorted compact rendering of the json formatted request body before forwarding it to the signer
+			var reqDump interface{}
+			err = json.Unmarshal(reqBody, &reqDump)
+			if err != nil {
+				log.Printf("error parsing http request body to json: %v", err)
+				return
 			}
+			// json.Marshal sorts the keys
+			sortedJson, _ := json.Marshal(reqDump)
+			var compactSortedJson bytes.Buffer
+			err = json.Compact(&compactSortedJson, sortedJson)
+
+			//requestChan <- append(compactSortedJson.Bytes())
+
 		} else {
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "http method not implemented"}`))
+			requestChan <- reqBody
+		}
+
+		// wait for response from ubirch backend to be forwarded
+		// TODO check performance
+		select {
+		case resp := <-responseChan:
+			w.WriteHeader(resp.Code)
+			w.Write(resp.Content)
 		}
 	}
 }
 
 type HTTPServer struct {
-	RequestChan  chan []byte
-	ResponseChan chan Response
+	ReceiveHandler  chan []byte
+	ResponseHandler chan Response
 }
 
 type Response struct {
@@ -49,7 +79,7 @@ func (srv *HTTPServer) Listen(endpoint string, ctx context.Context, wg *sync.Wai
 	defer wg.Done()
 
 	s := &http.Server{Addr: ":8080"}
-	http.HandleFunc(endpoint, handleRequest(srv.RequestChan, srv.ResponseChan))
+	http.HandleFunc(endpoint, handleRequest(srv.ReceiveHandler, srv.ResponseHandler))
 
 	go func() {
 		<-ctx.Done()
