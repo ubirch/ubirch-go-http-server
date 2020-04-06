e686d216e83b960d578c040bc3fe1d0b7fb865c0
Roxana Meixner <roxana.meixner@gmail.com>
2020-04-07 00:20:39 +0200

    use chi router

--- api/go.mod
+++ api/go.mod
@@ -2,4 +2,7 @@ module github.com/ubirch/ubirch-go-http-server/api
 
 go 1.13
 
-require github.com/google/uuid v1.1.1
+require (
+	github.com/go-chi/chi v4.1.0+incompatible
+	github.com/google/uuid v1.1.1
+)
--- api/ubirch_rest_api.go
+++ api/ubirch_rest_api.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"io/ioutil"
 	"log"
@@ -33,109 +34,8 @@ func forwardResponse(respChan chan HTTPResponse, w http.ResponseWriter) {
 	_, _ = w.Write(resp.Content)
 }
 
-func handleRequestHash(srv *HTTPServer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// only accept POST requests
-		if r.Method != "POST" {
-			http.Error(w, fmt.Sprintf("%s not implemented", r.Method), http.StatusNotImplemented)
-			return
-		}
-
-		// get UUID from URL path
-		id, err := uuid.Parse(strings.TrimPrefix(r.URL.Path, srv.Endpoint+"-hash/"))
-		if err != nil {
-			http.NotFound(w, r)
-			return
-		}
-
-		// check authorization
-		if XAuthToken(r) != srv.Auth {
-			http.Error(w, "invalid \"X-Auth-Token\"", http.StatusUnauthorized)
-			return
-		}
-
-		// make sure request body is of correct type
-		expectedType := "application/octet-stream"
-		if ContentType(r) != expectedType {
-			http.Error(w, fmt.Sprintf("Wrong content-type. Expected \"%s\"", expectedType), http.StatusBadRequest)
-			return
-		}
-
-		// read request body
-		message, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error reading request body: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		respChan := make(chan HTTPResponse)
-		srv.MessageHandler <- HTTPMessage{ID: id, Msg: message, IsAlreadyHashed: true, Response: respChan}
-
-		// wait for response from ubirch backend to be forwarded
-		forwardResponse(respChan, w)
-	}
-}
-
-func handleRequestData(srv *HTTPServer) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		// only accept POST requests
-		if r.Method != "POST" {
-			http.Error(w, fmt.Sprintf("%s not implemented", r.Method), http.StatusNotImplemented)
-			return
-		}
-
-		// get UUID from URL path
-		id, err := uuid.Parse(strings.TrimPrefix(r.URL.Path, srv.Endpoint+"/"))
-		if err != nil {
-			http.NotFound(w, r)
-			return
-		}
-
-		// check authorization
-		if XAuthToken(r) != srv.Auth {
-			http.Error(w, "invalid \"X-Auth-Token\"", http.StatusUnauthorized)
-			return
-		}
-
-		// make sure request body is of correct type
-		expectedType := "application/json"
-		if ContentType(r) != expectedType {
-			http.Error(w, fmt.Sprintf("Wrong content-type. Expected \"%s\"", expectedType), http.StatusBadRequest)
-			return
-		}
-
-		// read request body
-		reqBody, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error reading request body: %v", err), http.StatusBadRequest)
-			return
-		}
-
-		// generate a sorted compact rendering of the json formatted request body
-		var reqDump interface{}
-		var compactSortedJson bytes.Buffer
-		err = json.Unmarshal(reqBody, &reqDump)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("error parsing request body: %v", err), http.StatusBadRequest)
-			return
-		}
-		// json.Marshal sorts the keys
-		sortedJson, _ := json.Marshal(reqDump)
-		_ = json.Compact(&compactSortedJson, sortedJson)
-		message := compactSortedJson.Bytes()
-
-		// create HTTPMessage with individual response channel for each request
-		respChan := make(chan HTTPResponse)
-		srv.MessageHandler <- HTTPMessage{ID: id, Msg: message, IsAlreadyHashed: false, Response: respChan}
-
-		// wait for response from ubirch backend to be forwarded
-		forwardResponse(respChan, w)
-	}
-}
-
 type HTTPServer struct {
 	MessageHandler chan HTTPMessage
-	Endpoint       string
 	Auth           string
 }
 
@@ -152,22 +52,106 @@ type HTTPResponse struct {
 	Content []byte
 }
 
+func (srv *HTTPServer) handleRequestHash(w http.ResponseWriter, r *http.Request) {
+	// get UUID from URL
+	id, err := uuid.Parse(chi.URLParam(r, "uuid"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	// check authorization
+	if XAuthToken(r) != srv.Auth {
+		http.Error(w, "invalid \"X-Auth-Token\"", http.StatusUnauthorized)
+		return
+	}
+
+	// make sure request body is of correct type
+	expectedType := "application/octet-stream"
+	if ContentType(r) != expectedType {
+		http.Error(w, fmt.Sprintf("Wrong content-type. Expected \"%s\"", expectedType), http.StatusBadRequest)
+		return
+	}
+
+	// read request body
+	message, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	respChan := make(chan HTTPResponse)
+	srv.MessageHandler <- HTTPMessage{ID: id, Msg: message, IsAlreadyHashed: true, Response: respChan}
+
+	// wait for response from ubirch backend to be forwarded
+	forwardResponse(respChan, w)
+}
+
+func (srv *HTTPServer) handleRequestData(w http.ResponseWriter, r *http.Request) {
+	// get UUID from URL
+	id, err := uuid.Parse(chi.URLParam(r, "uuid"))
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	// check authorization
+	if XAuthToken(r) != srv.Auth {
+		http.Error(w, "invalid \"X-Auth-Token\"", http.StatusUnauthorized)
+		return
+	}
+
+	// make sure request body is of correct type
+	expectedType := "application/json"
+	if ContentType(r) != expectedType {
+		http.Error(w, fmt.Sprintf("Wrong content-type. Expected \"%s\"", expectedType), http.StatusBadRequest)
+		return
+	}
+
+	// read request body
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error reading request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	// generate a sorted compact rendering of the json formatted request body
+	var reqDump interface{}
+	var compactSortedJson bytes.Buffer
+	err = json.Unmarshal(reqBody, &reqDump)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("error parsing request body: %v", err), http.StatusBadRequest)
+		return
+	}
+	// json.Marshal sorts the keys
+	sortedJson, _ := json.Marshal(reqDump)
+	_ = json.Compact(&compactSortedJson, sortedJson)
+	message := compactSortedJson.Bytes()
+
+	// create HTTPMessage with individual response channel for each request
+	respChan := make(chan HTTPResponse)
+	srv.MessageHandler <- HTTPMessage{ID: id, Msg: message, IsAlreadyHashed: false, Response: respChan}
+
+	// wait for response from ubirch backend to be forwarded
+	forwardResponse(respChan, w)
+}
+
 func (srv *HTTPServer) Serve(ctx context.Context, wg *sync.WaitGroup) {
-	s := &http.Server{Addr: ":8080"}
-	http.HandleFunc(srv.Endpoint+"/", handleRequestData(srv))
-	http.HandleFunc(srv.Endpoint+"-hash/", handleRequestHash(srv))
+	router := chi.NewMux()
+	router.Post("/{uuid}", srv.handleRequestData)
+	router.Post("/{uuid}/hash", srv.handleRequestHash)
 
 	go func() {
 		<-ctx.Done()
-		log.Printf("shutting down http service (%s)", srv.Endpoint)
-		_ = s.Shutdown(ctx)
+		log.Printf("shutting down http server")
+		// TODO _ = s.Shutdown(ctx)
 	}()
 
 	go func() {
 		defer wg.Done()
-		err := s.ListenAndServe()
+		err := http.ListenAndServe(":8080", router)
 		if err != nil && err != http.ErrServerClosed {
-			log.Printf("error starting http service: %v", err)
+			log.Fatalf("error starting http service: %v", err)
 		}
 	}()
 }
